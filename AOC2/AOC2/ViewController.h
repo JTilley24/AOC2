17be3550017bfcc766cc39c89ed6947b78c5716d
JTilley24 <justin.tilley@me.com>
2013-06-12 21:10:54 -0400

    Week 2

---
+++ AOC2/AOC2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AOC2
+//
+//  Created by Justin Tilley on 6/12/13.
+//  Copyright (c) 2013 Justin Tilley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
